5fe86d234e89bcf2f7fe80158f708549f23910ea
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-11-09 00:24:12 +0530

    ✨ remove boilerplate

--- team-nature-ui/src/App.tsx
+++ team-nature-ui/src/App.tsx
@@ -1,38 +1,42 @@
-import * as React from "react"
+import "@fontsource/montserrat/700.css";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/300.css";
+
+import * as React from "react";
 import {
   ChakraProvider,
   Box,
+  Heading,
   Text,
-  Link,
   VStack,
-  Code,
   Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  extendTheme,
+} from "@chakra-ui/react";
+import { Logo } from "./Logo";
+import { Counter } from "./donations/Counter";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Montserrat",
+    body: "Montserrat",
+  },
+});
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
           <Logo h="40vmin" pointerEvents="none" />
+          <Heading>JOIN THE MOVEMENT</Heading>
           <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+            Help us remove 30 million pounds of trash by January 1st, 2022.
           </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+          <Heading as="h2" size="4xl">
+            <Counter from={0} to={1237109} />
+          </Heading>
         </VStack>
       </Grid>
     </Box>
   </ChakraProvider>
-)
+);
