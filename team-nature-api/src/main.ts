cb4d4e65e94e78de162a5097bca68fc9f21b4984
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-11-08 21:04:11 +0530

    ✨ cors

--- team-nature-api/src/main.ts
+++ team-nature-api/src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['localhost:3000'],
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(8080);
 }
